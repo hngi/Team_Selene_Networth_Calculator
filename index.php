e1fa0d2fdaa8a38ca828170f221ab6319dbcb7ab
Dave Nnamdi <51624310+davennamdi@users.noreply.github.com>
2019-09-24 08:22:20 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="font.css">
+    <title>Index Page</title>
+</head>
+<body>
+    <div class="wrap">
+        <div class="nav">
+            <div class="logo"> 
+                <a href="index.php">Selene</a>
+            </div>
+        </div> <!--end of nav div-->
+
+        <div class="header">
+            <h2>Selene Networth Calculator</h2>
+            <p>seamless calculations</p>
+            <a href="sign-up.php">Sign up</a>
+            <a href="sign-in.php" class="second">Sign in</a>
+
+        </div>
+    </div>
+</body>
+</html>
+
+
