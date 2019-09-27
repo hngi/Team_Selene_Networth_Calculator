b702226a0f0b0306836341d6f0ff78833930b6de
adeyemionward <adeyemiadeshina6@gmail.com>
2019-09-27 17:33:04 +0100

    css colors updated

--- dashboard.php
+++ dashboard.php
@@ -15,24 +15,24 @@
 </head>
 <div class="main__wrapper">
     <div>
-        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #F96D08;">
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#23C1CF;">
             <a class="navbar-brand" href="#">Selene</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
                 aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02" style="background-color:#23C1CF;">
                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li class="home-btn">
                         <a class="nav-link" href="index.php">Home</a>
                     </li>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="team.php">Our Team</a>
+                        <a class="nav-link" href="">Our Team</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="about.php">About Us</a>
+                        <a class="nav-link" href="">About Us</a>
                     </li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
